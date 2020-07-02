34d4550668ce656071c4ebdb7e1a91b877772603
NDI GLEN NFFORM <ndiglennform@gmail.com>
2020-07-02 15:50:37 +0100

    List the outputed times of travel for the time_stats docstring

--- Explore US Bikeshare data.py
+++ Explore US Bikeshare data.py
@@ -92,7 +92,12 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent times of travel.
+    Returns:
+        (int) month-most common months
+        (str) day-most common day
+        (int) hour-most common hour 
+    """
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
